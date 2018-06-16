60ea63c106341ae5e16ef6573b24e2ff20329e9c
alvarobilbao <bilbialvaro@gmail.com>
2018-06-16 16:08:20 -0400

    first commit, project initialization

---
+++ quiz-manager-core/src/main/java/fr/epita/quiz_manager/services/WhereClauseBuilder.java
@@ -0,0 +1,44 @@
+
+package fr.epita.quiz_manager.services;
+
+import java.util.Map;
+
+/**
+ * reserved for javadoc
+ */
+public class WhereClauseBuilder<T> {
+
+	private Map<String, Object> parameters;
+	private String queryString;
+
+	/**
+	 * @return the parameters
+	 */
+	public Map<String, Object> getParameters() {
+		return parameters;
+	}
+
+	/**
+	 * @param parameters
+	 *            the parameters to set
+	 */
+	public void setParameters(Map<String, Object> parameters) {
+		this.parameters = parameters;
+	}
+
+	/**
+	 * @return the queryString
+	 */
+	public String getQueryString() {
+		return queryString;
+	}
+
+	/**
+	 * @param queryString
+	 *            the queryString to set
+	 */
+	public void setQueryString(String queryString) {
+		this.queryString = queryString;
+	}
+
+}
