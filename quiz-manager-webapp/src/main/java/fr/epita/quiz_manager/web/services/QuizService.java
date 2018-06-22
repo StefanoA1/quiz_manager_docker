ccf502463757d8c7cf0e4a4995fad8d480579093
StefanoA1 <stefanoA1@users.noreply.github.com>
2018-06-22 17:46:13 +0200

    /create added for QuizService, quiz creation service

--- quiz-manager-webapp/src/main/java/fr/epita/quiz_manager/web/services/QuizService.java
+++ quiz-manager-webapp/src/main/java/fr/epita/quiz_manager/web/services/QuizService.java
@@ -1,8 +1,31 @@
 package fr.epita.quiz_manager.web.services;
 
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+import javax.jws.WebParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.springframework.http.MediaType;
+
+import fr.epita.quiz_manager.datamodel.Question;
+import fr.epita.quiz_manager.datamodel.Quiz;
+import fr.epita.quiz_manager.services.QuizOperationsService;
 
 @Path("/quizzes")
 public class QuizService {
-
+	@Inject
+	private QuizOperationsService quizServices;
+	
+	@POST
+	@Path("/create")
+	@Produces(value = {MediaType.APPLICATION_JSON_VALUE})
+	public boolean createQuiz(@WebParam Quiz criteria, @WebParam ArrayList<Question> questionsList) {
+		quizServices.createQuiz(criteria, questionsList);
+		return true;
+	}
+	
+	
 }
