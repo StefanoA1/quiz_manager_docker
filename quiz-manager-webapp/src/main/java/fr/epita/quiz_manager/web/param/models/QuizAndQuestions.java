1985b64ab23623e159c86ae123b6dbe7a103a911
StefanoA1 <stefanoA1@users.noreply.github.com>
2018-06-22 19:25:34 +0200

    QuizAndQuestions support class completed

--- quiz-manager-webapp/src/main/java/fr/epita/quiz_manager/web/param/models/QuizAndQuestions.java
+++ quiz-manager-webapp/src/main/java/fr/epita/quiz_manager/web/param/models/QuizAndQuestions.java
@@ -8,5 +8,29 @@ import fr.epita.quiz_manager.datamodel.Quiz;
 public class QuizAndQuestions {
 	private Quiz quiz;
 	private Set<Question> questionsSet;
+	/**
+	 * @return the quiz
+	 */
+	public Quiz getQuiz() {
+		return quiz;
+	}
+	/**
+	 * @param quiz the quiz to set
+	 */
+	public void setQuiz(Quiz quiz) {
+		this.quiz = quiz;
+	}
+	/**
+	 * @return the questionsSet
+	 */
+	public Set<Question> getQuestionsSet() {
+		return questionsSet;
+	}
+	/**
+	 * @param questionsSet the questionsSet to set
+	 */
+	public void setQuestionsSet(Set<Question> questionsSet) {
+		this.questionsSet = questionsSet;
+	}
 	
 }
