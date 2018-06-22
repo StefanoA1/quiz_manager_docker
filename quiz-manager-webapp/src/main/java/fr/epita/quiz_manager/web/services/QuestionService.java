57e0b280f262f3fbf78052a6243896f2ba85eb38
alvarobilbao <bilbialvaro@gmail.com>
2018-06-21 20:39:40 -0400

    update question query

--- quiz-manager-webapp/src/main/java/fr/epita/quiz_manager/web/services/QuestionService.java
+++ quiz-manager-webapp/src/main/java/fr/epita/quiz_manager/web/services/QuestionService.java
@@ -1,5 +1,23 @@
 package fr.epita.quiz_manager.web.services;
 
+import java.util.List;
+
+import javax.jws.WebParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import fr.epita.quiz_manager.datamodel.MCQChoice;
+import fr.epita.quiz_manager.datamodel.Question;
+
+@Path("/questions")
 public class QuestionService {
 
+	
+	
+	@POST
+	@Path("/create")
+	public void create(@WebParam Question q, @WebParam List<MCQChoice> answers) {
+		
+	}
+	
 }
