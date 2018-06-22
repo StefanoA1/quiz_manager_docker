b471023843670d664d4b1a24d41baaa509da1e23
alvarobilbao <bilbialvaro@gmail.com>
2018-06-22 19:20:43 -0400

    mcq choice query

--- quiz-manager-webapp/src/main/java/fr/epita/quiz_manager/web/services/QuestionService.java
+++ quiz-manager-webapp/src/main/java/fr/epita/quiz_manager/web/services/QuestionService.java
@@ -7,10 +7,12 @@ import javax.jws.WebParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import org.springframework.http.MediaType;
 
+import fr.epita.quiz_manager.datamodel.MCQChoice;
 import fr.epita.quiz_manager.datamodel.Question;
 import fr.epita.quiz_manager.services.QuestionOperationsService;
 import fr.epita.quiz_manager.web.param.models.MCQQuestion;
@@ -33,6 +35,15 @@ public class QuestionService {
 	@Produces(value = {MediaType.APPLICATION_JSON_VALUE})
 	public void create(@WebParam MCQQuestion question) {
 		questionOperationsService.createMCQQuestion(question.getQuestion(), question.getAnswers());
+	}
+	@GET
+	@Path("/{id}")
+	@Produces(value = {MediaType.APPLICATION_JSON_VALUE})
+	public List<MCQChoice> getMCQChoices(@PathParam("id") Integer id) {
+		Question question = new Question();
+		question.setId(id);
+		
+		return questionOperationsService.searchMCQChoices(question);
 	}
 	
 }
